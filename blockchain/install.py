3cc12e98bf2a252d1c581b096ae08f075be6301d
mayfieldmobster <raecd123@gmail.com>
2022-02-18 13:17:36 +0000

    basic install for testing

--- blockchain/install.py
+++ blockchain/install.py
@@ -1,4 +1,5 @@
 import os
+import node
 
 """
 install dependencies
@@ -15,20 +16,22 @@ input = input("To Accept type 'ACCEPT'  : ")
 if input != "ACCEPT":
     exit()
 
+print("Your Private key is need to verify that your public key belongs to you make sure you are using the official version of deci")
+priv_key = input("Private Key: ")
 print("This is the the key that will be rewarded")
 pub_key = input("Public Key: ")
 print("Bellow insert Port that will be forwarded for communication (Leave Blank to Default)")
 norm_port = input("Port: ")
-print("Bellow insert another port that you will have to forwarded (Leave Blank to Default)")
-ai_port = input("Port: ")
+if not norm_port:
+    norm_port = "1379"
 
 with open("./info/Public_key.txt", "w") as file:
     file.write(pub_key)
 
-with open("./info/ports.txt", "w") as file:
-    file.write(norm_port + "\n" + ai_port)
 
 print("That's you,\n"
       "make sure you are port forwarding BOTH PORTS!!!,\n"
       "run DECI.exe to start,\n"
       "if you want to change anything run this again.\n")
+
+node.announce(pub_key, norm_port, node.__version__, "blockchain", priv_key)
