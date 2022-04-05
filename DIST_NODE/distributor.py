2e447220b25142291d7b7eb20e30e395108350b0
mayfieldmobster <raecd123@gmail.com>
2022-04-05 23:49:55 +0100

    started dist_node

---
+++ DIST_NODE/distributor.py
@@ -0,0 +1,12 @@
+import node
+
+#send to all non dist nodes
+
+#updated blockchain node write system
+
+"""
+functions that need to be run:
+ONLINE?
+NREQ
+ALL NODE UPDATE COMMANDS
+"""
---
+++ DIST_NODE/reciever.py
@@ -0,0 +1,15 @@
+import node
+
+
+def rec(my_ip):
+    print("---RECEIVER STARTED---")
+    while True:
+        message, address = node.receive(my_ip)
+        print(f"Message from {address} , {message}\n")
+        with open("recent_messages.txt", "a") as file:
+            file.write(f"{address[0]} {' '.join(message)}\n")
+
+
+
+if __name__ == "__main__":
+    rec("192.168.68.112")
