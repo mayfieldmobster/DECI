c49ee8c2ef0b77e472df6d7276c2599fdd46a666
mayfieldmobster <raecd123@gmail.com>
2022-06-20 18:31:56 +0100

    test node update

--- tools/pickle_writer.py
+++ tools/pickle_writer.py
@@ -1,8 +1,8 @@
 import pickle
 
 node1 = {"time": 1645188735.3090425, "ip": "192.168.68.140", "pub_key": "c917f27bfb852e6667036c9f44767a226d4f8105ad3c9ea09370f8d0", "port": 1379, "version": 1.0, "node_type": "Blockchain"}
-node2 = {"time": 1645188735.3090446, "ip": "192.168.68.145", "pub_key": "0b3553c4f5d86f76ef1b686b953bce9f69922c8c1141fb9b1a5c892f", "port": 1379, "version": 1.0, "node_type": "Blockchain"}
+node2 = {"time": 1645188735.3090432, "ip": "192.168.68.144", "pub_key": "0b3553c4f5d86f76ef1b686b953bce9f69922c8c1141fb9b1a5c892f", "port": 1379, "version": 1.0, "node_type": "Blockchain"}
 node3 = {"time": 1645188735.3090457, "ip": "192.168.68.112", "pub_key": "93419305da43adb7b91b6d4f0526ccda8130a9385b9c6b2acf7f38a1", "port": 1379, "version": 1.0, "node_type": "dist"}
 
-with open("../DIST_NODE/info/Nodes.pickle", "wb") as file:
+with open("../Blockchain/info/Nodes.pickle", "wb") as file:
     pickle.dump([node1, node2, node3], file)
